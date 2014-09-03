0522589527370db1dce873db23eb606169e18db4
viracsreng <srengvirac@hotmail.com>
2014-09-03 14:48:13 +0200

    initial commit

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var azure = require('azure');
-var adm = require ('adm-zip');
+var fs = require('fs');
+var storage_account = 'gallerie';
+var gallerieKey = 'SiQVY98VhO+NI1m6jfBMgB1M/00geM/puCgpMpRvsBSUz0H/xcgF77Wx9SiD7buJFvXZ9NTvyRNvf200CNT6Kg==';
+var package_container = 'packages';
+var index_container = 'descriptifs';
+var admzip = require('adm-zip');
+var blobService = azure.createBlobService(storage_account,gallerieKey);
+var packages_folder = __dirname + '/databases/packages/';
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.use("/databases", express.static(__dirname + '/databases'));
+app.use("/controllers", express.static(__dirname + '/controllers'));
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -62,8 +71,47 @@ app.use(function(err, req, res, next) {
     });
 });
 
+function Init(){
+	// Get index.json
+	blobService.getBlobToFile(index_container, 'index.json', __dirname + '/databases/index.json', function(error, result, response){
+	});
+}
+
+app.get('/gallery/product/download', function(req, res){
+	var item = url.parse(req.url).query;
+	var filePath = packages_folder + item + '.zip';
+	blobService.getBlobToFile(package_container, item + '.mob', filePath, function(error, result, response){
+		if(!error){
+		    var returnHeaders = {};
+			returnHeaders['Content-Disposition'] = 'attachment; filename="'+ item +'.zip"';
+			returnHeaders['Content-Type'] = 'application/zip';
+			res.writeHead(200, returnHeaders); 
+			var stream = fs.createReadStream(filePath);
+			stream.on('open', function () {				
+				stream.pipe(res);
+			});
+			stream.on('end', function () {
+				res.end();
+			});			
+		}	
+	});			
+});
+
+app.get('/gallery/product/install', function(req, res){
+    var item = url.parse(req.url).query;
+	var filePath = packages_folder + item + '.zip';
+	
+	blobService.getBlobToFile(package_container, item + '.mob', filePath, function(error, result, response){
+		if(!error){
+			var zip = new admzip(filePath);
+			zip.extractAllTo('\Extracted\\' + item, true);
+			res.redirect(req.get('referer'));
+		}
+	});	
+});
+
+Init();
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
-
 module.exports = app;
