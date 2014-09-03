6ce9563c524ef9a77c6a9113584d5aa6bfdcb136
viracsreng <srengvirac@hotmail.com>
2014-09-03 15:07:59 +0200

    initial commit

--- app.js
+++ app.js
@@ -13,62 +13,26 @@ var index_container = 'descriptifs';
 var admzip = require('adm-zip');
 var blobService = azure.createBlobService(storage_account,gallerieKey);
 var packages_folder = __dirname + '/databases/packages/';
+var url = require('url');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
 app.use("/databases", express.static(__dirname + '/databases'));
 app.use("/controllers", express.static(__dirname + '/controllers'));
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
+
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', routes);
 app.get('/', function (req, res){
 	res.render('index.html');
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
 });
 
 function Init(){
